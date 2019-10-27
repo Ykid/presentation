8f8e29ffbed06f3f47325f705db7c8df76baeeb9
David Cai <david.cai@plutux.com>
2019-10-27 22:57:51 +0800

    add rating plots

--- count_reviews.py
+++ count_reviews.py
@@ -10,11 +10,41 @@ for t in country_tuples:
     df = pd.read_csv(file_path)
     country_stat_dict[country_name] = len(df)
 
-plt.figure(figsize=(15, 10))
-pd.Series(country_stat_dict).sort_values(ascending=False).plot.bar(
-    color=["#83ffd0", "#c69aff", "#f4e0b0", "#f467ae", "#f26464"])
-plt.xticks(rotation=50)
-plt.xlabel("District")
-plt.ylabel("Number of Reviews")
-plt.show()
-# plt.savefig("./results/mostrecent/review_stat.png")
+ratings_dict = {
+    'Canada': 4.8,
+    'Australia': 4.7,
+    'HongKong': 4.7,
+    'United_States': 4.7,
+    'Philippines': 4.6,
+    'ca': 4.6,
+    'India': 4.6,
+}
+
+
+def plot_reviews(series: pd.Series, path: str = None):
+    plt.figure(figsize=(15, 10))
+    series.sort_values(ascending=False).plot.bar(
+        color=["#83ffd0", "#c69aff", "#f4e0b0", "#f467ae", "#f26464"])
+    plt.xticks(rotation=50)
+    plt.xlabel("District")
+    plt.ylabel("Number of Reviews")
+    if not path:
+        plt.show()
+    else:
+        plt.savefig(path)
+
+
+def plot_ratings(series: pd.Series, path: str = None):
+    plt.figure(figsize=(15, 10))
+    series.sort_values(ascending=False).plot()
+    plt.xticks(rotation=50)
+    plt.xlabel("District")
+    plt.ylabel("App Rating")
+    if not path:
+        plt.show()
+    else:
+        plt.savefig(path)
+
+
+plot_reviews(pd.Series(country_stat_dict), "./results/mostrecent/review_stat.png")
+plot_ratings(pd.Series(ratings_dict), "./results/mostrecent/rating_stat.png")
