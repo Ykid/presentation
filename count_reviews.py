4179b6c8bf63d76b4f3192b6221be99765e60cab
David Cai <david.cai@plutux.com>
2019-10-28 22:45:16 +0800

    use bigger axis label to improve readability and add mean rating comparison

--- count_reviews.py
+++ count_reviews.py
@@ -3,14 +3,21 @@ import matplotlib.pyplot as plt
 from countries import *
 
 country_tuples = list(eng_country_dic.items()) + list(cn_country_dic.items())
-country_stat_dict = {}
-for t in country_tuples:
-    country_name = t[0]
-    file_path = f'./reviews/{country_name}/reviews_{country_name}_2019_10_27.csv'
-    df = pd.read_csv(file_path)
-    country_stat_dict[country_name] = len(df)
 
-ratings_dict = {
+# for x axis tick display
+abbreviation = {
+    'Mainland_China': "CN",
+    'Australia': "AU",
+    'HongKong': "HK",
+    'United_States': "US",
+    'Philippines': "PH",
+    'Canada': "CA",
+    'India': "IN",
+}
+
+country_stat_dict = {}
+country_mean_rating = {}
+total_ratings_dict = {
     'Mainland_China': 4.8,
     'Australia': 4.7,
     'HongKong': 4.7,
@@ -19,15 +26,29 @@ ratings_dict = {
     'Canada': 4.6,
     'India': 4.6,
 }
+lines = []
+columns = ["country", "recent rating mean", "version range", "# collected reviews"]
+for t in country_tuples:
+    country_name = t[0]
+    file_path = f'./reviews/{country_name}/reviews_{country_name}_2019_10_27.csv'
+    df = pd.read_csv(file_path)
+    abbr = abbreviation[country_name]
+    country_stat_dict[abbr] = len(df)
+    country_mean_rating[abbr] = df.rating.mean()
+    lines.append([abbr, df.rating.mean(), f'{df.version.min()}-{df.version.max()}', len(df)])
+
+df = pd.DataFrame(lines, columns=columns)
+df.to_csv("./results/review_stat.csv", index=False)
 
 
 def plot_reviews(series: pd.Series, path: str = None):
     plt.figure(figsize=(15, 10))
-    series.sort_values(ascending=False).plot.bar(
-        color=["#83ffd0", "#c69aff", "#f4e0b0", "#f467ae", "#f26464"])
-    plt.xticks(rotation=50)
-    plt.xlabel("District")
-    plt.ylabel("Number of Reviews")
+    ax = series.sort_values(ascending=False).plot.bar()
+    plt.tick_params(axis='both', which='major', labelsize=30)
+    plt.xticks(rotation=0)
+    plt.xlabel("District", fontsize=40)
+    plt.grid(axis='y')
+    plt.ylabel("Number of Reviews", fontsize=40)
     if not path:
         plt.show()
     else:
@@ -37,14 +58,15 @@ def plot_reviews(series: pd.Series, path: str = None):
 def plot_ratings(series: pd.Series, path: str = None):
     plt.figure(figsize=(15, 10))
     series.sort_values(ascending=False).plot()
-    plt.xticks(rotation=50)
-    plt.xlabel("District")
-    plt.ylabel("App Rating")
+    plt.tick_params(axis='both', which='major', labelsize=30)
+    plt.xticks(rotation=0)
+    plt.xlabel("District", fontsize=40)
+    plt.ylabel("Average App Rating", fontsize=40)
     if not path:
         plt.show()
     else:
         plt.savefig(path)
 
 
-plot_reviews(pd.Series(country_stat_dict), "./results/mostrecent/review_stat.png")
-plot_ratings(pd.Series(ratings_dict), "./results/mostrecent/rating_stat.png")
+plot_reviews(pd.Series(country_stat_dict), "./results/review_stat.png")
+plot_ratings(pd.Series(country_mean_rating), "./results/rating_stat.png")
